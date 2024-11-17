dcfadb3331f4d130cedbaf64113c13f3da703653
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-11-17 19:23:52 -0300

    feat: create project backend and frontend

---
+++ linkup-backend/src/main/java/tech/silva/linkup/backend/LinkupBackendApplication.java
@@ -0,0 +1,13 @@
+package tech.silva.linkup.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkupBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkupBackendApplication.class, args);
+	}
+
+}
---
+++ linkup-backend/src/test/java/tech/silva/linkup/backend/LinkupBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.silva.linkup.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkupBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
